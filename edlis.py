ef95ac2ed3bcdb1b3dd39063880561228212eda4
Clarice <57488906+Clarise342@users.noreply.github.com>
2021-04-17 18:03:44 +0900

    Update edlis.py

--- edlis.py
+++ edlis.py
@@ -1,9 +1,10 @@
 #python 3.8.0
 #coding: utf-8
 
-import discord, sys
+import discord, sys, os
 from discord.ext import tasks, commands
 
+token = os.environ["TOKEN"]
 intents = discord.Intents.default()
 intents.members = True
 
@@ -34,31 +35,4 @@ class Edlis(commands.Bot):
 if __name__ == "__main__":
   bot = Edlis(command_prefix="e:")
   
-  @bot.command()
-  async def exit(ctx):
-    await ctx.message.edit(delete_after=5.0)
-    await bot.logout()
-    await sys.exit()
-  
-  @bot.command(name="reload")
-  async def cogreload(ctx):
-    await ctx.message.edit(delete_after=5.0)
-    for c in extensions:
-      try:
-        bot.unload_extension(c)
-      except:
-        pass
-      
-    scs = []
-    for c in extensions:
-      try:
-        bot.load_extension(c)
-      except Exception as e:
-        print(f"Error >> Could not load extension {c}")
-        print(e)
-      else:
-        scs.append(c)
-    await ctx.send(f"☑️ {len(extensions)} 個の内、{len(scs)} 個のコグのreloadが完了しました",delete_after=10.0)
-    return print("reloadが完了しました")
-  
-  bot.run('ODEyMzE3MDA5ODQ5MjIxMTgw.YC-_Kw.lZrpdWqoks_J6jlh7m9-t92GX5E')
+  bot.run('TOKEN')
